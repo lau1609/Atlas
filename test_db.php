735a5d702d132f94054b8eac2a5c25168ca13c8b
Erick Blanco <e159erick123@gmail.com>
2025-07-01 11:33:32 -0700

    Se Elimnaron archivos basura
    Pues asi y ajam

--- test_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once("connectMySql.php");
-
-if ($connectMySql->connect_error) {
-    die("Error de conexión: " . $connectMySql->connect_error);
-} else {
-    echo "✅ Conexión exitosa a la base de datos.";
-}
-?>
